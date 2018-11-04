a836f2fca92b95e9c453ce7e6eb2d2998cf46a67
Priyanka Sharma <prisharma1910@gmail.com>
2018-11-04 13:51:35 +0530

    updated code for moving block

--- src/moving-block/MovingBlock.js
+++ src/moving-block/MovingBlock.js
@@ -2,10 +2,10 @@ import React from 'react';
 import "./MovingBlock.css";
 
 class MovingBlock extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            position : 1
+            position: 1
             //1 - top left
             //2 - top right
             //3 - bottom left
@@ -22,17 +22,22 @@ class MovingBlock extends React.Component {
     }
 
     getSvgRect = () => {
-        return {outerSquare: this.outerSquare.getBoundingClientRect(),
-            innerSquare: this.innerSquare.getBoundingClientRect()};
+        if (!this.outerSquare) {
+            return { outerSquare: {}, innerSquare: {} };
+        }
+        return {
+            outerSquare: this.outerSquare.getBoundingClientRect(),
+            innerSquare: this.innerSquare.getBoundingClientRect()
+        };
     }
 
     updateInnerSvg = () => {
-        const { outerSquare, innerSquare } = this.getSvgRect();        
+        const { outerSquare, innerSquare } = this.getSvgRect();
 
         let newx = null;
         let newy = null;
 
-        switch(this.state.position){
+        switch (this.state.position) {
             case 1:
             default:
                 newx = 0;
@@ -47,92 +52,67 @@ class MovingBlock extends React.Component {
                 newy = outerSquare.height - innerSquare.height;
                 break;
             case 4:
-                newx= 0;
+                newx = 0;
                 newy = outerSquare.height - innerSquare.height;
         }
-        
+
         this.innerSquare.setAttribute('x', newx);
         this.innerSquare.setAttribute('y', newy);
     }
 
     moveForward = () => {
-        const { outerSquare, innerSquare } = this.getSvgRect();
-
-        let newx = null;
-        let newy = null;
-        let position = 0;
-
-        if (outerSquare.x === innerSquare.x) {
-            if (outerSquare.y === innerSquare.y) {
-                newx = outerSquare.width - innerSquare.width;
-                position = 2;
-            } else {
-                newy = 0;
-                position = 1;
-            }
-        } else {
-            if (outerSquare.y === innerSquare.y) {
-                newy = outerSquare.height - innerSquare.height;
-                position = 3;
-            } else {
-                newx = 0;
-                position = 4;
-            }
+        let currentPosition = this.state.position;
+        let newPosition = currentPosition + 1;
+        if (currentPosition === 4) {
+            newPosition = 1;
         }
-        if (newx !== null) {
-            this.innerSquare.setAttribute('x', newx);
-        } else {
-            this.innerSquare.setAttribute('y', newy);
-        }
-
         this.setState({
-            position
-        });
+            position: newPosition
+        })
     }
 
     moveBackward = () => {
-        const { outerSquare, innerSquare } = this.getSvgRect();        
-
-        let newx = null;
-        let newy = null;
-        let position = 0
-
-        if (outerSquare.x === innerSquare.x) {
-            if (outerSquare.y === innerSquare.y) {
-                newy = outerSquare.height - innerSquare.height;
-                position = 4;
-            } else {
-                newx = outerSquare.width - innerSquare.width;
-                position = 3;
-            }
-        } else {
-            if (outerSquare.y === innerSquare.y) {
-                newx = 0;
-                position = 1;
-            } else {
-                newy = 0;
-                position = 2;
-            }
+        let currentPosition = this.state.position;
+        let newPosition = currentPosition - 1;
+        if (currentPosition === 1) {
+            newPosition = 4;
         }
-        if (newx !== null) {
-            this.innerSquare.setAttribute('x', newx);
-        } else {
-            this.innerSquare.setAttribute('y', newy);
-        }
-
         this.setState({
-            position
-        });
+            position: newPosition
+        })
+    }
+
+    getBoxPosition = () => {
+        let boxCoordinates = { boxX: 0, boxY: 0 }; // position 1
+        const { outerSquare, innerSquare } = this.getSvgRect();
+
+        switch (this.state.position) {
+            case 2:
+                boxCoordinates.boxX = outerSquare.width - innerSquare.width;
+                break;
+            case 3:
+                boxCoordinates.boxX = outerSquare.width - innerSquare.width;
+                boxCoordinates.boxY = outerSquare.height - innerSquare.height;
+                break;
+            case 4:
+                boxCoordinates.boxY = outerSquare.height - innerSquare.height;
+                break;
+            default:
+                boxCoordinates.boxX = 0;
+                boxCoordinates.boxY = 0;
+        }
+        return boxCoordinates;
     }
 
     render() {
+        const { boxX, boxY } = this.getBoxPosition();
         return (<React.Fragment>
             <h2> Block </h2>
             <div className="float-left">
                 <svg x="0" y="0" width="50vw" height="50vh" ref={(ref) => { this.svgContainer = ref; }}>
                     <rect width="100%" height="100%" ref={(ref) => { this.outerSquare = ref; }}
                         style={{ fill: '#fff', strokeWidth: 3, stroke: '#006400' }} />
-                    <rect x="0" y="0" width="100" height="100" ref={(ref) => { this.innerSquare = ref; }}
+                    <rect x={boxX} y={boxY} width="100" height="100" ref={(ref) => { this.innerSquare = ref; }}
                         style={{ fill: '#000' }} />
                 </svg>
             </div>
